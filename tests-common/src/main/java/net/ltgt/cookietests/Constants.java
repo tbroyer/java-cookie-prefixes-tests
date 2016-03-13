568b71f0f99aca305674e1104797a27980c025b6
Thomas Broyer <t.broyer@ltgt.net>
2016-03-13 17:13:29 +0100

    Update to new naming: using double-underscore prefix instead of dollar

--- tests-common/src/main/java/net/ltgt/cookietests/Constants.java
+++ tests-common/src/main/java/net/ltgt/cookietests/Constants.java
@@ -1,7 +1,7 @@
 package net.ltgt.cookietests;
 
 public interface Constants {
-  String TEST_COOKIE_NAME = "$Secure-Foo";
+  String TEST_COOKIE_NAME = "__Secure-Foo";
   String TEST_COOKIE_VALUE = "value";
 
   String TEST_COOKIE = TEST_COOKIE_NAME + "=" + TEST_COOKIE_VALUE;
